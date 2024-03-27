ef20f93f2d8031f364c265707cfc82fd243e2222
john <johngemmell124@gmail.com>
2024-03-27 15:54:36 -0400

    use models + basic dao

---
+++ backend/src/daos/userDoa.js
@@ -0,0 +1,17 @@
+import userModel from '../models/userModel';
+
+export const getUserById = (userId) => {
+  return userModel.findById(userId);
+};
+
+export const createUser = (user) => {
+  return userModel.create(user);
+};
+
+export const updateUser = (userId, user) => {
+  return userModel.updateOne({ _id: userId }, { $set: user });
+};
+
+export const deleteUser = (userId) => {
+  return userModel.deleteOne({ _id: userId });
+};
