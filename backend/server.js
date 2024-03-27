442357f4ff8b616a3c6288447019bc97cbbf5450
john <johngemmell124@gmail.com>
2024-03-27 15:22:57 -0400

    Set up db connection for backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,43 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+// constansts
+const DB_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/coolcats';
+const PORT = process.env.PORT || 4000;
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+
+mongoose.connect(DB_URL);
 
 const app = express();
 
-const PORT = 4000;
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
 
+const sessionOptions = {
+  secret: 'secretpassword',
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true,
+  };
+}
+
+// middleware
+app.use(session(sessionOptions));
+app.use(express.json());
+
+// routes
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
