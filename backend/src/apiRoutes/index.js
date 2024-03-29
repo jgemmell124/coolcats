4bde23472a5971f5bca57bac597a4018c7aaf12f
john <johngemmell124@gmail.com>
2024-03-29 18:26:21 -0400

    User routes working

---
+++ backend/src/apiRoutes/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userRoutes from './userRoutes.js';
+
+
+const router = express.Router();
+
+router.use('/users', userRoutes);
+
+
+
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import apiRoutes from './apiRoutes/index.js';
 
-// constansts
+// constants
 const DB_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/coolcats';
 const PORT = process.env.PORT || 4000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
@@ -20,11 +21,13 @@ app.use(
 );
 
 const sessionOptions = {
-  secret: 'secretpassword',
-  resave: false,
-  saveUninitialized: false,
+  name: 'coolcats.sid', // name of the cookie
+  secret: 'secretpassword', // a secret key used to sign the session ID cookie
+  resave: false, //don't save session if unmodifed
+  saveUninitialized: false, // don't create session until something stored
 };
 
+// NOTE: make sure your node env is set to 'development' locally
 if (process.env.NODE_ENV !== 'development') {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -32,14 +35,15 @@ if (process.env.NODE_ENV !== 'development') {
     secure: true,
   };
 }
-
 // middleware
 app.use(session(sessionOptions));
 app.use(express.json());
 
 // routes
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.use('/api', apiRoutes);
+
+app.use('*', (_req, res) => {
+  return res.sendStatus(404);
 });
 
 app.listen(PORT, () => {
