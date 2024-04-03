9919661024a3030e01d6eade37153d2808edeb16
john <johngemmell124@gmail.com>
2024-04-03 17:59:52 -0400

    sandwich routes
    auto populate db locally
    
    remove version key from database

--- backend/src/apiRoutes/index.js
+++ backend/src/apiRoutes/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import userRoutes from './userRoutes.js';
-
+import sandwichRoutes from './sandwichRoutes.js';
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
-
-
-
+router.use('/sandwiches', sandwichRoutes);
 
 export default router;
