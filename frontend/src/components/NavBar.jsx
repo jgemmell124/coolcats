52a7ef5a663a665dbe4c3416ddb73c5f653b8dde
john <johngemmell124@gmail.com>
2024-04-05 16:00:49 -0400

    moved search bar around

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 
 const NavBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
-  const title = 'Nvbar title';
+  const title = 'Navbar title';
+  const isSignedIn = true;
 
   const links = [
     { title: 'Home', url: '#' },
-    { title: 'Features', url: '#' },
-    { title: 'Pricing', url: '#' },
-    { title: 'About', url: '#' },
+    { title: 'Sandwiches', url: '#' },
+    ...(isSignedIn ? 
+      [
+        { title: 'Profile', url: '#' },
+        { title: 'Sign Out', url: '#' },
+      ] :
+      [{ title: 'Sign In', url: '#' }]
+    ),
   ];
 
   const handleSearch = (event) => {
@@ -30,18 +36,15 @@ const NavBar = () => {
         className={`${isCollapsed ? 'collapse' : ''} navbar-collapse justify-content-between`}
         id='navbarColor01' 
       >
-        <ul className='navbar-nav mr-auto'>
-          {links.map((link, index) => (
-            <li className='nav-item' key={index}>
-              <a className='nav-link' href={link.url}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
         <form 
           className='form-inline'
-          style={{ marginRight: '15px', transition: 'linear' }}>
+          style={{ margin: '5px' }}>
           <div className='input-group'>
-            <input className='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search'/>
+            <input 
+              className='form-control mr-sm-2'
+              type='search' 
+              placeholder='Find sandwiches or users...' 
+              aria-label='Search'/>
             <button
               className='btn btn-outline-info  my-sm-0'
               type='submit'
@@ -51,6 +54,13 @@ const NavBar = () => {
             </button>
           </div>
         </form>
+        <ul className='navbar-nav mr-auto'>
+          {links.map((link, index) => (
+            <li className='nav-item' key={index}>
+              <a className='nav-link' href={link.url}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
