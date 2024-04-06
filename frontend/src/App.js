e0d7a177b89aecba2a68e235c0d7291131b6ee71
jgemmell124 <78321702+jgemmell124@users.noreply.github.com>
2024-04-05 20:49:22 -0400

    11 sign in (#31)
    * login page
    
    * create account link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home';
 import NotFound from './pages/NotFound';
 import Navigation from './components/Navigation';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/login' element={<h1>Log in here</h1>}/>
+        <Route exact path='/login' element={<LoginPage />}/>
         <Route exact path='/signup' element={<h1>Sign up here</h1>}/>
         <Route path='*' element={<MainContent />} />
       </Routes>
