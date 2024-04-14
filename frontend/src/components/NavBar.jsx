02b268f4ed43f354b309d11cea20a6e513d5a988
giladavniheller <57957977+giladavniheller@users.noreply.github.com>
2024-04-14 12:47:49 -0400

    oops

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -97,168 +97,96 @@ const NavBar = () => {
     </li>
   );
 
-  const yeah = true;
-  if (yeah) {
-    return (
+  return (
+    <Stack
+      direction='row'
+      spacing={2}
+      sx={{
+        backgroundColor: '#090910',
+        height: '75px',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <a className='navbar-brand' href='#' style={{ marginRight: '10%' }}>
+        <img width='250px' src='WolliesboxdLogoLight.png' alt={'Wolliesboxd'} />
+      </a>
       <Stack
         direction='row'
         spacing={2}
         sx={{
           backgroundColor: '#090910',
           height: '75px',
-          justifyContent: 'center',
+          justifyContent: 'space-around',
           alignItems: 'center',
         }}
       >
-        <a className='navbar-brand' href='#' style={{ marginRight: '10%' }}>
-          <img
-            width='250px'
-            src='WolliesboxdLogoLight.png'
-            alt={'Wolliesboxd'}
-          />
-        </a>
-        <Stack
-          direction='row'
-          spacing={2}
+        <Button
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          style={{
+            fontFamily: 'Gill Sans',
+            fontSize: '18px',
+          }}
           sx={{
-            backgroundColor: '#090910',
-            height: '75px',
-            justifyContent: 'space-around',
-            alignItems: 'center',
+            color: open ? 'white' : '#b2b2b4ff',
+            ':hover': {
+              color: 'white',
+            },
           }}
         >
-          <Button
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup='true'
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            style={{
-              fontFamily: 'Gill Sans',
-              fontSize: '18px',
-            }}
-            sx={{
-              color: open ? 'white' : '#b2b2b4ff',
-              ':hover': {
-                color: 'white',
-              },
-            }}
-          >
-            <img
-              style={{
-                marginRight: '5px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-              }}
-              width='35px'
-              height='35px'
-              src='emptyProfilePic.png'
-              alt={'profilePicture'}
-            />
-            {username}
-            <ArrowDropDownIcon />
-          </Button>
-          <Menu
-            id='basic-menu'
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <MenuItem onClick={handleClose}>Home</MenuItem>
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleSignout();
-                handleClose();
-              }}
-            >
-              Logout
-            </MenuItem>
-          </Menu>
-          <NavBarButton text='Home' onClickFunction={() => {}} />
-          <NavBarButton text='Sandwiches' onClickFunction={() => {}} />
-          <NavBarButton text='Your Stats' onClickFunction={() => {}} />
-          <Button
-            variant='contained'
-            sx={{
-              backgroundColor: '#52bf30',
-              ':hover': { backgroundColor: '#42852d' },
-            }}
-          >
-            <AddIcon />
-            Log Sandwich
-          </Button>
-        </Stack>
-      </Stack>
-    );
-  } else {
-    return (
-      <nav className='navbar navbar-expand-md navbar-dark bg-dark justify-contnent-between'>
-        <a className='navbar-brand' href='#'>
           <img
-            width='250px'
-            src='WolliesboxdLogoLight.png'
-            alt={'Wolliesboxd'}
+            style={{
+              marginRight: '5px',
+              borderRadius: '50%',
+              objectFit: 'cover',
+            }}
+            width='35px'
+            height='35px'
+            src='emptyProfilePic.png'
+            alt={'profilePicture'}
           />
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          onClick={() => setIsCollapsed(!isCollapsed)}
-          style={{ marginRight: '15px' }}
-          aria-label='Toggle navigation'
+          {username}
+          <ArrowDropDownIcon />
+        </Button>
+        <Menu
+          id='basic-menu'
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
         >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div
-          className={`${isCollapsed ? 'collapse' : ''} navbar-collapse justify-content-between`}
-          id='navbarColor01'
+          <MenuItem onClick={handleClose}>Home</MenuItem>
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleSignout();
+              handleClose();
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+        <NavBarButton text='Home' onClickFunction={() => {}} />
+        <NavBarButton text='Sandwiches' onClickFunction={() => {}} />
+        <NavBarButton text='Your Stats' onClickFunction={() => {}} />
+        <Button
+          variant='contained'
+          sx={{
+            backgroundColor: '#52bf30',
+            ':hover': { backgroundColor: '#42852d' },
+          }}
         >
-          <form className='form-inline' style={{ margin: '5px' }}>
-            <div className='input-group'>
-              <input
-                className='form-control mr-sm-2'
-                type='search'
-                placeholder='Find sandwiches or users...'
-                aria-label='Search'
-              />
-              <button
-                className='btn btn-outline-info  my-sm-0'
-                type='submit'
-                onClick={handleSearch}
-              >
-                Search
-              </button>
-            </div>
-          </form>
-          <ul style={{ marginRight: '15px' }} className='navbar-nav'>
-            <li className='nav-item'>
-              <Link className='nav-link' to={'/'}>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to={'/'}>
-                Sandwiches
-              </Link>
-            </li>
-            {isAuthenticated ? (
-              accountDropdown
-            ) : (
-              <li className='nav-item'>
-                <Link className='nav-link' to={'/login'}>
-                  Sign In
-                </Link>
-              </li>
-            )}
-          </ul>
-        </div>
-        <Alert alert={alert} setAlert={setAlert} />
-      </nav>
-    );
-  }
+          <AddIcon />
+          Log Sandwich
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default NavBar;
